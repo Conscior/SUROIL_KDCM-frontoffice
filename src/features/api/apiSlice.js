90f0cf329857e5c1b4b73682316ab270cb10e43d
Conscior <abderr.ryad@gmail.com>
2023-01-02 19:05:09 +0100

    env variables support

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, clearCredentials } from "../state/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
+  baseUrl: import.meta.env.VITE_BACKEND_URL || "http://localhost:3500",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
